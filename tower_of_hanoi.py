72b8661787b250ef803e22c69c8e1695fa719cae
Amir Farhadi <amirf147@gmail.com>
2020-10-11 12:53:38 +0300

    Update tower_of_hanoi.py

--- tower_of_hanoi.py
+++ tower_of_hanoi.py
@@ -1,67 +1,67 @@
-##Question 11 (Bonus):
-##
-##The idea of the puzzle is that you have three
-##pegs and a set of disks where each disk is a different size. The goal
-##is to move all of the disks(i.e., 3) from peg 1 to peg 3 while not
-##violating the following rules:
-##
-##1. You may only move 1 disk at a time.
-##2. A larger disk cannot be placed on top of a smaller disk.
-##3. All disks must be on some peg except the disk in-transit.
+# This solution shows the tracing of the function calls and also tells you
+# how many moves were performed to solve the puzzle given the number of disks
 
-peg1 = []
-peg2 = []
-peg3 = []
+A = []
+B = []
+C = []
+indent_count = 0
+moves = 0
 
-num_disks = int(input('How many disks are in this puzzle?: '))
+n = int(input("num disks: "))
+
+if ((type(n) == int) and (n > 0)):
+
+    A = [i + 1 for i in range(n)] # peg size represented by number in list
+    A = A[::-1] # order of numbers is largest to smallest
 
 
-if ((type(num_disks) == int) and (num_disks > 0)):
+def move(n, start, end, bridge, indent_count):
 
-    print(num_disks)
-
-    peg1 = [i + 1 for i in range(num_disks)] # peg size represented by number in list
-    peg1 = peg1[::-1] # order of numbers is largest to smallest
-
-    count = 0;
-    i = 0;
-    j = 1;
-
-    pegs = [peg1, peg2, peg3] #2d array for the pegs
+    global moves
+    indent_count += 1
+    print(("    " * indent_count) + "ENTER", n)
     
-    if num_disks % 2 == 0:
+    
+    if n > 0:
 
-        while count != 15: #len(peg3) != num_disks:
+        moves += 1
+        
+        print(("    " * indent_count) + "Calling move(" + str(n-1) + ", " +
+              str(start) + ", " + str(bridge) + ", " + str(end) + ")")
 
-            if i > 2:
-                i = 0
-                print("i is 0")
+        
+        # Move n - 1 disks from start to end to free bottom one
+        move(n - 1, start, bridge, end, indent_count)
 
-            if j > 2:
-                j = 0
-                
 
-            if not pegs[1]:#pegs[i][-1] < max(pegs[j]):
-                
-                pegs[j].append(pegs[i].pop())
-                count += 1
+        print(("    " * indent_count) + "Moving to " + str(end) +
+              " from " + str(start))
 
-            elif pegs[1]:
-                print('pegs[%d] : %s' % (j, pegs[j]))
+        # Move disk #n to end from start
+        end.append(start.pop())
 
-                if pegs[i][-1] < max(pegs[j]):
-                    pegs[j].append(pegs[i].pop())
-                    count += 1
+        # Show current state of pegs
+        print(("    " * indent_count) + "A = " + str(A) + " B = " + str(B) +
+              " C = " + str(C))
 
-            if not pegs[2]:
-                pegs[2].append(pegs[i].pop())
-                count += 1
+        
+        print(("    " * indent_count) + "Calling move(" + str(n-1) +
+              ", " + str(bridge) + ", " +str(end) + ", " +
+              str(start) + ")")
+        
+        # Move to end the n - 1 number of disks remaining on bridge
+        move(n - 1, bridge, end, start, indent_count)
 
-            elif pegs[2]:
-                pegs[2].append(pegs[i].pop())
-                count += 1;
-                
-            print(pegs)
-            i += 1
-            #j += 1
-            
+    print(("    " * indent_count) + "EXIT", n)
+    indent_count -= 1
+        
+
+
+
+print("Calling move(" + str(n) + ", " + str(A) + ", " +
+              str(C) + ", " + str(B))
+
+#Call the function with start peg being A, end C and the bridge B
+move(n, A, C, B, indent_count)
+
+print("The number of moves =", moves)
